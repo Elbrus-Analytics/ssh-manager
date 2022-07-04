5196ccc1ef9ce98593da6bc3b11d7b0d2a3ad8a1
elbrus <Tobias Schmidt>
2022-07-04 17:11:58 +0200

    added a option for juming host

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from sqlite3 import connect
 from sys import stderr
 from os import getenv
+from xxlimited import Str
 from paramiko import AutoAddPolicy, SSHClient
 from dotenv import load_dotenv
 import time
+
+import paramiko
 
 def main():
     load_dotenv()
@@ -10,11 +14,11 @@ def main():
         vars = load_environment_variables()
     except UnconfiguredEnvironment as e:
         exit(e)
-    print(vars['JUMPHOST_IP'], vars['JUMPHOST_PORT'], vars['JUMPHOST_USER'], vars['JUMPHOST_PASS'])
-    ssh_connection = establish_connection(vars['JUMPHOST_IP'], vars['JUMPHOST_PORT'], vars['JUMPHOST_USER'], vars['JUMPHOST_PASS'])
-    command = 'sh ip int brief\n'
-    execute_command(ssh_connection, command)
+    
+    ssh_connection = establish_connection_using_jumphost('14.14.14.28', 'cisco', 'cisco')
+    execute_command(ssh_connection, 'sh ip int brief\n')
     close_connection(ssh_connection)
+
 
 class UnconfiguredEnvironment(Exception):
     '''Exception class for unconfigured environment variables'''
@@ -22,6 +26,9 @@ class UnconfiguredEnvironment(Exception):
 
 
 def load_environment_variables() -> dict[str, str]:
+    '''
+    method used to load environment variables
+    '''
     environment_variables_list = ['JUMPHOST_IP', 'JUMPHOST_PORT', 'JUMPHOST_USER', 'JUMPHOST_PASS']
     vars = dict()
     for var in environment_variables_list:
@@ -31,7 +38,35 @@ def load_environment_variables() -> dict[str, str]:
     return vars
 
 
-def establish_connection(host:str, port:int, user:str, key:str) -> SSHClient:
+def establish_connection_using_jumphost(target:str, tuser:str, tkey:str, port=22) -> SSHClient:
+    '''
+    method used to connect to a target, using a jumphost inbetween
+    '''
+    jumphost_ssh_connection = establish_connection_to_jumphost(True)
+    time.sleep(.5)
+    jumphost_ssh_connection.send('ssh -l '+tuser+' -p '+str(port)+' '+target+'\n')
+    jumphost_ssh_connection.recv(65535)
+    time.sleep(2)
+    jumphost_ssh_connection.send(tkey+'\n')
+    jumphost_ssh_connection.recv(65535)
+    time.sleep(.5)
+    return jumphost_ssh_connection
+
+
+def establish_connection_to_jumphost(mute=False) -> SSHClient:
+    '''
+    method used to establish a connection to the jumphost
+    '''
+    load_dotenv()
+    try:
+        vars = load_environment_variables()
+    except UnconfiguredEnvironment as e:
+        exit(e)
+    ssh_connection = establish_connection(vars['JUMPHOST_IP'], vars['JUMPHOST_USER'], vars['JUMPHOST_PASS'], vars['JUMPHOST_PORT'], mute)
+    return ssh_connection
+
+
+def establish_connection(host:str, user:str, key:str, port=22, mute=False) -> SSHClient:
     '''
     method used to establish a connection using the address, port, username and password
     return a established connection
@@ -40,7 +75,9 @@ def establish_connection(host:str, port:int, user:str, key:str) -> SSHClient:
     ssh_connection_pre.set_missing_host_key_policy(AutoAddPolicy())
     ssh_connection_pre.connect(hostname=host, port=port, username=user, password=key)
     ssh_connection = ssh_connection_pre.invoke_shell()
-    print((ssh_connection.recv(65535)).decode("utf-8"))
+    output = ssh_connection.recv(65535)
+    if not mute:
+        print(output.decode("utf-8"))
     return ssh_connection
 
 
