d5ea9cc9aca46ffa67476b8ce570be7398e7c7ab
elbrus <Tobias Schmidt>
2022-07-04 12:24:51 +0200

    implemented a invoke shell

--- ssh.py
+++ ssh.py
@@ -1,3 +1,4 @@
+from sys import stderr
 from paramiko import AutoAddPolicy, SSHClient
 import time
 
@@ -7,28 +8,40 @@ def main():
     username = 'cisco'
     password = 'cisco'
 
-    command = 'show ip int brief \n'
 
     ssh_connection = establish_connection(hostname, port, username, password)
+    command = 'sh ip int brief\n'
     execute_command(ssh_connection, command)
     close_connection(ssh_connection)
 
 
 def establish_connection(host:str, port:int, user:str, key:str) -> SSHClient:
-    ssh_connection = SSHClient()
-    ssh_connection.set_missing_host_key_policy(AutoAddPolicy())
-    ssh_connection.connect(hostname=host, port=port, username=user, password=key)
+    '''
+    method used to establish a connection using the address, port, username and password
+    return a established connection
+    '''
+    ssh_connection_pre = SSHClient()
+    ssh_connection_pre.set_missing_host_key_policy(AutoAddPolicy())
+    ssh_connection_pre.connect(hostname=host, port=port, username=user, password=key)
+    ssh_connection = ssh_connection_pre.invoke_shell()
+    print((ssh_connection.recv(65535)).decode("utf-8"))
     return ssh_connection
 
 
 def execute_command(ssh_connection:SSHClient, command:str) -> None:
-    stdin, stdout, stderr = ssh_connection.exec_command(command)
-    stdin.close()
-    output = stdout.readlines()
-    print(' '.join(map(str, output)))
+    '''
+    method used to execute a command using a established connection, prints the output from the command
+    '''
+    ssh_connection.send(command)
+    time.sleep(.5)
+    print(ssh_connection.recv(65535).decode("utf-8"))
+    
 
 
 def close_connection(ssh_connection:SSHClient) -> None:
+    '''
+    method used to close a connection
+    '''
     ssh_connection.close()
 
 
