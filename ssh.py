b920eb848834c9bc093680aeb0a70d1890eb5411
elbrus <Tobias Schmidt>
2022-07-04 10:35:38 +0200

    added methods

--- ssh.py
+++ ssh.py
@@ -1,22 +1,36 @@
 from paramiko import AutoAddPolicy, SSHClient
 import time
 
-hostname = '10.0.0.1'
-port = 22
-username = 'cisco'
-password = 'cisco'
+def main():
+    hostname = '10.0.0.1'
+    port = 22
+    username = 'cisco'
+    password = 'cisco'
 
-command = 'show ip int brief \n'
+    command = 'show ip int brief \n'
 
-ssh_connection = SSHClient()
-ssh_connection.set_missing_host_key_policy(AutoAddPolicy())
-ssh_connection.connect(hostname=hostname, port=port, username=username, password=password)
+    ssh_connection = establish_connection(hostname, port, username, password)
+    execute_command(ssh_connection, command)
+    close_connection(ssh_connection)
 
 
-stdin, stdout, stderr = ssh_connection.exec_command(command)
-stdin.close()
-output = stdout.readlines()
-print(' '.join(map(str, output)))
+def establish_connection(host:str, port:int, user:str, key:str) -> SSHClient:
+    ssh_connection = SSHClient()
+    ssh_connection.set_missing_host_key_policy(AutoAddPolicy())
+    ssh_connection.connect(hostname=host, port=port, username=user, password=key)
+    return ssh_connection
 
-ssh_connection.close()
-print("working")
+
+def execute_command(ssh_connection:SSHClient, command:str) -> None:
+    stdin, stdout, stderr = ssh_connection.exec_command(command)
+    stdin.close()
+    output = stdout.readlines()
+    print(' '.join(map(str, output)))
+
+
+def close_connection(ssh_connection:SSHClient) -> None:
+    ssh_connection.close()
+
+
+if __name__=="__main__":
+    main()
