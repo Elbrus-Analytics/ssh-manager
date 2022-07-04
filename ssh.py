ac7d8e8a4f8430c8a44a81039fc05ab7a0e97e33
elbrus <Tobias Schmidt>
2022-07-04 09:59:27 +0200

    connection is working

--- ssh.py
+++ ssh.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from paramiko import AutoAddPolicy, SSHClient
+import time
+
+hostname = '10.0.0.1'
+port = 22
+username = 'cisco'
+password = 'cisco'
+
+command = 'show ip int brief \n'
+
+ssh_connection = SSHClient()
+ssh_connection.set_missing_host_key_policy(AutoAddPolicy())
+ssh_connection.connect(hostname=hostname, port=port, username=username, password=password)
+
+
+stdin, stdout, stderr = ssh_connection.exec_command(command)
+stdin.close()
+output = stdout.readlines()
+print(' '.join(map(str, output)))
+
+ssh_connection.close()
+print("working")
