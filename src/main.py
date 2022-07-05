f5b3f085694405bb3245106fdc4bd21bb5243adb
ssh-script <ssh-script@elbrus-analytics.at>
2022-07-05 19:10:18 +0200

    extended documentation, minor changes in methods

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
+from audioop import add
 from os import getenv
+from os.path import isdir
+from os import mkdir
 from paramiko import AutoAddPolicy, SSHClient
 from datetime import date
 from dotenv import load_dotenv
 import time
-import paramiko
 
-path = ''
+directory = '../config/'
+address = None
 
 def main():  
     ssh_connection = establish_connection_using_jumphost('14.14.14.28', 'cisco', 'cisco')
-    execute_command(ssh_connection, 'term len 0')
-    save_output(ssh_connection, 'sh ip int brief')
+    save_output(ssh_connection, 'sh ip prot')
+    close_connection(ssh_connection)
+    ssh_connection = establish_connection_to_jumphost()
+    save_output(ssh_connection, 'sh ip prot')
+    close_connection(ssh_connection)
     pass
 
 
@@ -22,6 +28,8 @@ class UnconfiguredEnvironment(Exception):
 def load_environment_variables() -> dict[str, str]:
     '''
     method used to load environment variables
+
+    :return dict[str, str]: a dict where the environment variables name is mapped to the value
     '''
     environment_variables_list = ['JUMPHOST_IP', 'JUMPHOST_PORT', 'JUMPHOST_USER', 'JUMPHOST_PASS']
     vars = dict()
@@ -35,8 +43,16 @@ def load_environment_variables() -> dict[str, str]:
 def establish_connection_using_jumphost(target:str, tuser:str, tkey:str, port=22) -> SSHClient:
     '''
     method used to connect to a target, using a jumphost inbetween
+
+    :param str target: ip address of target
+    :param str tuser: username to log into target
+    :param str tkey: password to log into target
+    :param int port: port to connect to
+
+    :return SSHClient: ssh session with target
     '''
-    jumphost_ssh_connection = establish_connection_to_jumphost(True)
+    global address
+    jumphost_ssh_connection = establish_connection_to_jumphost()
     time.sleep(.5)
     jumphost_ssh_connection.send('ssh -l '+tuser+' -p '+str(port)+' '+target+'\n')
     jumphost_ssh_connection.recv(65535)
@@ -44,40 +60,64 @@ def establish_connection_using_jumphost(target:str, tuser:str, tkey:str, port=22
     jumphost_ssh_connection.send(tkey+'\n')
     jumphost_ssh_connection.recv(65535)
     time.sleep(.5)
+    address = target.replace('.','-')
+    #!!!!!
+    jumphost_ssh_connection.send('term len 0\n')
+    #!!!!!
     return jumphost_ssh_connection
 
 
-def establish_connection_to_jumphost(mute=False) -> SSHClient:
+def establish_connection_to_jumphost() -> SSHClient:
     '''
     method used to establish a connection to the jumphost
+
+    :param boolean mute: set True to mute the output 
+
+    :return SSHClient: ssh session with target
     '''
     load_dotenv()
     try:
         vars = load_environment_variables()
     except UnconfiguredEnvironment as e:
         exit(e)
-    ssh_connection = establish_connection(vars['JUMPHOST_IP'], vars['JUMPHOST_USER'], vars['JUMPHOST_PASS'], vars['JUMPHOST_PORT'], mute)
+    ssh_connection = establish_connection(vars['JUMPHOST_IP'], vars['JUMPHOST_USER'], vars['JUMPHOST_PASS'], vars['JUMPHOST_PORT'])
     return ssh_connection
 
 
-def establish_connection(host:str, user:str, key:str, port=22, mute=False) -> SSHClient:
+def establish_connection(host:str, user:str, key:str, port=22) -> SSHClient:
     '''
     method used to establish a connection using the address, port, username and password
     return a established connection
+
+    :param str host: IP-Address of the target
+    :param str user: username to log into the target
+    :param str key: password to log into the target
+    :param int port: port to connect to
+    :param boolean mute: set True to mute the output
+
+    :return SSHClient: ssh session with target
     '''
+    global address
+    address = host.replace('.','-')
     ssh_connection_pre = SSHClient()
     ssh_connection_pre.set_missing_host_key_policy(AutoAddPolicy())
     ssh_connection_pre.connect(hostname=host, port=port, username=user, password=key)
     ssh_connection = ssh_connection_pre.invoke_shell()
-    output = ssh_connection.recv(65535)
-    if not mute:
-        print(output.decode("utf-8"))
+    #!!!!!
+    ssh_connection.send('term len 0\n')
+    #!!!!!
     return ssh_connection
 
 
 def execute_command(ssh_connection:SSHClient, command:str, readsize=65535) -> str:
     '''
     method used to execute a command using a established connection, prints the output from the command
+
+    :param SSHClient ssh_connection: active ssh connection to a target
+    :param str command: command to execute
+    :param int readsize: how many bytes of the response are read
+
+    :return str: the response the the comand decoded in 'utf-8'
     '''
     ssh_connection.send(command+'\n')
     time.sleep(10)
@@ -88,6 +128,8 @@ def execute_command(ssh_connection:SSHClient, command:str, readsize=65535) -> st
 def close_connection(ssh_connection:SSHClient) -> None:
     '''
     method used to close a connection
+
+    :param SSHClient ssh_connection: active ssh connection to a target
     '''
     ssh_connection.close()
 
@@ -95,10 +137,19 @@ def close_connection(ssh_connection:SSHClient) -> None:
 def save_output(ssh_connection:SSHClient, command:str, readsize=65535) -> None:
     '''
     method used to execute a command and save its output to a file, following the naming format "hostname-YYYY-MM-DD.txt" for the output file
+
+    :param SSHClient ssh_connection: active ssh connection to a target
+    :param str command: command to execute
+    :param int readsize: how many bytes of the response are read
     '''
+    global address
     response = execute_command(ssh_connection, command, readsize)
-    hostname = response.split('\n')[-1][:-1]+'-'+str(date.today())+'.txt'
-    file = open(path+hostname, 'w')
+    path = directory+address
+    print(path)
+    if not isdir(path):
+        mkdir(path)
+    path +='/'+command.replace(' ','-')+'.txt'
+    file = open(path, 'w')
     file.write(response)
     file.close()
 
